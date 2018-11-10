1db7047e9e0518bdfcfef9bf6d2a6ab8a97044ac
Udaikaran Singh <www.udaikaran@gmail.com>
2018-11-09 16:00:52 -0800

    Update architecture1_cnn.py

--- architecture1_cnn.py
+++ architecture1_cnn.py
@@ -31,24 +31,24 @@ import os
 
 
 class arch1_cnn(nn.Module):
-	def __init__(self):
-		super(arch1_cnn, self).__init__()
-		
-		#convolutional layer 1
-		self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 12, kernel_size = 8)
-		self.conv1_normed = nn.BatchNorm2d(12)
-		torch_init.xavier_normal_(self.conv1.weight)
+    def __init__(self):
+        super(arch1_cnn, self).__init__()
+        
+        #convolutional layer 1
+        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 12, kernel_size = 8)
+        self.conv1_normed = nn.BatchNorm2d(12)
+        torch_init.xavier_normal_(self.conv1.weight)
+        
+        #convolution layer 2
+        self.conv2 = nn.Conv2d(in_channels = 12, out_channels = 10, kernel_size = 8)
+        self.conv2_normed = nn.BatchNorm2d(10)
+        torch_init.xavier_normal_(self.conv2.weight)
 
-		#convolution layer 2
-		self.conv2 = nn.Conv2d(in_channels = 12, out_channels = 10, kernel_size = 8)
-		self.conv2_normed = nn.BatchNorm2d(10)
-		torch_init.xavier_normal_(self.conv2.weight)
-
-		#convolutional layer 3
-		self.conv3 = nn.Conv2d(in_channels = 10, out_channels = 8, kernel_size = 6)
-		self.conv3_normed = nn.BatchNorm2d(8)
-		torch_init.xavier_normal_(self.conv3.weight)
-
+        #convolutional layer 3
+        self.conv3 = nn.Conv2d(in_channels = 10, out_channels = 8, kernel_size = 6)
+        self.conv3_normed = nn.BatchNorm2d(8)
+        torch_init.xavier_normal_(self.conv3.weight)
+        
         #max-pooling
         self.pool = nn.MaxPool2d(kernel_size = 4, stride = 4)
 
@@ -68,29 +68,29 @@ class arch1_cnn(nn.Module):
 
 
 
-	def forward(self, batch):
-		
-		#convolutional layers
-		batch = func.relu(self.conv1_normed(self.conv1(batch)))
-		batch = func.relu(self.conv2_normed(self.conv2(batch)))
-		batch = func.sigmoid(self.conv3_normed(self.conv3(batch)))
+    def forward(self, batch):
+        
+        #convolutional layers
+        batch = func.relu(self.conv1_normed(self.conv1(batch)))
+        batch = func.relu(self.conv2_normed(self.conv2(batch)))
+        batch = func.sigmoid(self.conv3_normed(self.conv3(batch)))
 
-		#pooling
-		batch = self.pool(batch)
+        #pooling
+        batch = self.pool(batch)
 
-		#flattening data
-		batch = batch.view(-1, self.num_flat_features(batch))
+        #flattening data
+        batch = batch.view(-1, self.num_flat_features(batch))
 
-		#fully connected layers
-		batch = func.relu(self.fc1(batch))
-		batch = func.relu(self.fc2(batch))
-		batch = self.fc3(batch)
+        #fully connected layers
+        batch = func.relu(self.fc1(batch))
+        batch = func.relu(self.fc2(batch))
+        batch = self.fc3(batch)
 
-		return func.sigmoid(batch)
+        return func.sigmoid(batch)
 
 
 
-	def num_flat_features(self, inputs):
+    def num_flat_features(self, inputs):
         
         # Get the dimensions of the layers excluding the inputs
         size = inputs.size()[1:]
